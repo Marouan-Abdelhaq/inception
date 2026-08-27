0c26fb66e06aac8582bd755617fe3b3a2e113e20
Marouan Abdelhaq <abdelhaqmarouan70@gmail.com>
2026-08-27 03:15:50 +0100

    corrige le projet

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -18,6 +18,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo "Bootstrapping database, user and privileges"
 
 	mariadbd --user=mysql --bootstrap --datadir=/var/lib/mysql <<-EOSQL
+		FLUSH PRIVILEGES;
 		CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 		GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
